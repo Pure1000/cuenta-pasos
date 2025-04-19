307df11008195a2bb2f7a81f0d8087c7de9efa52
Pure1000 <purebionest@gmail.com>
2025-04-19 15:43:30 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,15 @@
+datalogger.onLogFull(function () {
+    for (let index = 0; index < 4; index++) {
+        music.play(music.stringPlayable("C D E F G A B C5 ", 120), music.PlaybackMode.UntilDone)
+        music.play(music.stringPlayable("C5 B A G F E D C ", 120), music.PlaybackMode.UntilDone)
+    }
+})
 input.onGesture(Gesture.Shake, function () {
     Count += 1
+    datalogger.log(datalogger.createCV("Pasos", Count))
 })
 let Count = 0
+Count = 0
 basic.forever(function () {
     basic.showNumber(Count)
 })
